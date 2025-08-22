e6bb02f8fc84c6f5412fccaba25626635d0752a3
kbaikov <konstantin.baikov@gmail.com>
2025-08-22 08:23:23 +0200

    Init day01

---
+++ day01/src/part1.rs
@@ -0,0 +1,39 @@
+/// influenced by https://github.com/ChristopherBiscardi/advent-of-code/blob/main/2024/rust/day-01/src/part1.rs
+use std::{error::Error, fs, iter::zip};
+type GenericError = Box<dyn Error + Send + Sync + 'static>;
+
+fn main() {
+    let _ = process(&fs::read_to_string("day01_input.txt").unwrap());
+}
+
+fn process(input: &str) -> Result<String, GenericError> {
+    let mut left = vec![];
+    let mut right = vec![];
+    for line in input.lines() {
+        let mut items = line.split_whitespace();
+        left.push(items.next().unwrap().parse::<isize>().unwrap());
+        right.push(items.next().unwrap().parse::<isize>().unwrap());
+    }
+    left.sort();
+    right.sort();
+
+    let result: usize = zip(left, right).map(|(l, r)| l.abs_diff(r)).sum();
+    dbg!(result);
+    Ok(result.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn test_one() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("11", process(input).unwrap());
+    }
+}
